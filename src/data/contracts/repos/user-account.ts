2f423605a902fec73d15fe0ad67c923469f6c524
robertotics4 <robertooliveiragp@gmail.com>
2022-07-13 14:58:33 -0300

    ensure Service calls CreateFacebookAccountRepo when LoadUserAccountRepository returns undefined

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -1,9 +1,24 @@
 export interface LoadUserAccountRepository {
-  load: (params: LoadUserAccountRepository.Params) => Promise<void>
+  load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
 
 export namespace LoadUserAccountRepository {
   export type Params = {
     email: string
   }
+
+  export type Result = undefined
+}
+
+export interface CreateFacebookAccountRepository {
+  createFromFacebook: (params: LoadUserAccountRepository.Params) => Promise<void>
+}
+
+export namespace CreateFacebookAccountRepository {
+  export type Params = {
+    facebookId: string
+    email: string
+    name: string
+  }
+
 }
