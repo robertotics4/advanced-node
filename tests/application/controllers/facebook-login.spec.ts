a193422b801dcc1a963779ccdb6786a1571ad730
robertotics4 <robertooliveiragp@gmail.com>
2022-07-18 18:09:33 -0300

    ensure FacebookLoginController returns 400 if token is undefined

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -34,4 +34,15 @@ describe('FacebookLoginController', () => {
       data: new Error('The field token is required')
     })
   })
+
+  it('should return 400 if token is undefined', async () => {
+    const sut = new FacebookLoginController()
+
+    const httpResponse = await sut.handle({ handle: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field token is required')
+    })
+  })
 })
