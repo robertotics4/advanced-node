c303423892768ea828b73aa4a38039d5638a8f49
robertotics4 <robertooliveiragp@gmail.com>
2022-07-18 11:26:34 -0300

    ensure PgUserAccountRepository update account if id is defined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -57,7 +57,7 @@ describe('PgUserAccountRepository', () => {
       sut = new PgUserAccountRepository()
     })
 
-    it('should create an accoun tif id is undefined', async () => {
+    it('should create an account if id is undefined', async () => {
       await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
@@ -66,6 +66,30 @@ describe('PgUserAccountRepository', () => {
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
       expect(pgUser?.id).toBe(1)
+    })
+
+    it('should update account if id is defined', async () => {
+      await pgUserRepo.save({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_fb_id'
+      })
+
+      await sut.saveWithFacebook({
+        id: '1',
+        email: 'new_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
+
+      const pgUser = await pgUserRepo.findOne({ id: 1 })
+
+      expect(pgUser).toEqual({
+        id: 1,
+        email: 'any_email',
+        name: 'new_name',
+        facebookId: 'new_fb_id'
+      })
     })
   })
 })
