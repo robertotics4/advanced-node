55e8860279cd5c0dfece953027d25e26ef643ee2
robertotics4 <robertooliveiragp@gmail.com>
2022-07-25 17:26:45 -0300

    ensure AuthenticationMiddleware returns 403 if authorization header was not provided

---
+++ src/main/factories/middlewares/authentication.ts
@@ -0,0 +1,9 @@
+import { AuthenticationMiddleware } from '@/application/middlewares'
+import { setupAuthorize } from '@/domain/useCases'
+import { makeJwtTokenHandler } from '../crypto'
+
+export const makeAuthenticationMiddleware = (): AuthenticationMiddleware => {
+  const authorize = setupAuthorize(makeJwtTokenHandler())
+
+  return new AuthenticationMiddleware(authorize)
+}
