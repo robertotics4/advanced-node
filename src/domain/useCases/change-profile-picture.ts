cbd9d4faedddd2c14c855b73363e0bf3da5a132f
robertotics4 <robertooliveiragp@gmail.com>
2022-07-29 18:36:00 -0300

    fix LoadUserProfile output to be undefined also

--- src/domain/useCases/change-profile-picture.ts
+++ src/domain/useCases/change-profile-picture.ts
@@ -18,7 +18,7 @@ export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfil
     pictureUrl: file !== undefined
       ? await fileStorage.upload({ file, key })
       : undefined,
-    name: file === undefined ? (await userProfileRepo.load({ id })).name : undefined
+    name: file === undefined ? (await userProfileRepo.load({ id }))?.name : undefined
   }
 
   const userProfile = new UserProfile(id)
