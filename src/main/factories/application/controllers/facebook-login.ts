13a138d430d331007244fca324172b22f84d6928
robertotics4 <robertooliveiragp@gmail.com>
2022-08-01 09:30:51 -0300

    refactor directories

--- src/main/factories/application/controllers/facebook-login.ts
+++ src/main/factories/application/controllers/facebook-login.ts
@@ -1,5 +1,5 @@
 import { FacebookLoginController } from '@/application/controllers'
-import { makeFacebookAuthentication } from '@/main/factories/useCases/facebook-authentication'
+import { makeFacebookAuthentication } from '@/main/factories/domain/useCases/facebook-authentication'
 
 export const makeFacebookLoginController = (): FacebookLoginController => {
   return new FacebookLoginController(makeFacebookAuthentication())
