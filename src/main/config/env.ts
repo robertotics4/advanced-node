a1988d088e32cc09a367492148b49f34191862af
robertotics4 <robertooliveiragp@gmail.com>
2022-08-02 09:24:28 -0300

    add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,6 +5,11 @@ export const env = {
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'c02b1e197ee6aec2947b193148cdaef7',
     accessToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? '',
+    secret: process.env.AWS_S3_SECRET ?? '',
+    bucket: process.env.BUCKET ?? ''
+  },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'ff2çej2çlk22çlkej'
 }
